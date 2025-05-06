21a06f2853eae25e316e45b0029196ae0a6fcd00
jonathankaretsky <73963949+jonathankaretsky@users.noreply.github.com>
2025-05-06 00:10:53 -0400

    New ReadMe. Old one was default creat react reaadme

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
+
+const API_URL = 'http://www.omdbapi.com?apikey=915c49b1';
+
+function App() {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        if (data.Search) {
+            setMovies(data.Search);
+        }
+    };
+
+    useEffect(() => {
+        searchMovies({searchTerm});
+    }, []);
+
+    return (
+        <div className="app">
+            <h1>MovieLand</h1>
+
+            <div className="search">
+                <input
+                    placeholder="Search For Movies"
+                    value={searchTerm}
+                onChange = {(e) => setSearchTerm(e.target.value)}
+                    />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+
+            {movies.length > 0 ? (
+                <div className="container">
+                    {movies.map((movie) => (
+                        <MovieCard movie={movie} />
+                    ))}
+                </div>
+            ) : (
+                <div className="empty">
+                    <h1>No Movies Found</h1>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default App;
